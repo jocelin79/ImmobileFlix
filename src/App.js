8918303ec1f6a9d99607b4c032232d18701a94e8
Jocelin Nunes <jocelinnunes@gmail.com>
2022-03-02 19:10:22 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default () => {
       </section>
 
       <footer>
-        <p>Feito com <span role="img aria-label=oração">❤️</span> por Jocelin Nunes.
+        <p>Feito com <span role="img aria-label=coração">❤️</span> por Jocelin Nunes.
         Dados do site Themovinedb.org</p>
         <img src={"https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"} alt={"tmdg logo"}></img>
       </footer>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const cors = require("cors");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-app.use(cors());
