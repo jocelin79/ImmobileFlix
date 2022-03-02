653dddd7684efd5f6a0cfb35659259b8f9af2f8c
Jocelin Nunes <jocelinnunes@gmail.com>
2022-03-02 18:12:58 -0300

    Adiciona Procfile

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const cors = require("cors");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+app.use(cors());
